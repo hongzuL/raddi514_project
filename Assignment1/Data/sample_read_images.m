fdc101c1cbcec17e70a0d40814dff0c91459453b
Hong Zu-lab <hongzu@ualberta.ca>
2018-10-03 18:53:11 -0600

    snr

--- Assignment1/Data/sample_read_images.m
+++ Assignment1/Data/sample_read_images.m
@@ -2,6 +2,7 @@ clear all
 %Sample Script for Assignment 1
 us_img = imread('dataset1_img_hip.png'); % Load the image
 mask   = imread('dataset1_mask_hip.png'); % Load the mask of regions of interest
+bg_mask = imread('dataset1_bg_mask_hip.png'); % Load the background mask of regions of interest
 % Convert Image to grayscale
 us_img = rgb2gray(us_img);
 mask   = rgb2gray(mask);
@@ -9,7 +10,7 @@ mask   = rgb2gray(mask);
 % noise removal
 img_rm_noise = medfilt2(us_img);
 % Contrast Enhancement
-img_adjust = imadjust(img_rm_noise,[0.15 0.5],[0 1]);
+img_adjust = imadjust(img_rm_noise,[0.1 0.5]);
 % Apply threshold
 img = img_adjust;
 % threshold = mean(mean(img));
@@ -45,23 +46,35 @@ rec_img_log = img-log_k;
 % Calcualte Indices
     % Convert to double if necesary
     % Multiply by mask
+%
 
 % Display
 figure(1),
 subplot(1,3,1); imshow(us_img,[]),title('Original')
-subplot(1,3,2); imshow(img_rm_noise,[]),title('adjust contrast')
-subplot(1,3,3); imshow(img_adjust,[]),title('remove noise')
+subplot(1,3,2); imshow(img_rm_noise,[]),title('remove noise')
+subplot(1,3,3); imshow(img_adjust,[]),title('adjust contrast')
 figure(2)
 subplot(2,2,1); imshow(rec_img_sobel,[]),title('sobel')
 subplot(2,2,2); imshow(rec_img_lap4,[]),title('lap 4')
 subplot(2,2,3); imshow(rec_img_lap8,[]),title('lap 8')
 subplot(2,2,4); imshow(rec_img_log,[]),title('log')
+
 % SNR
-[peaksnr_sobel, snr_sobel] = psnr(uint8(rec_img_sobel), mask);
-fprintf("%f, %f\n",peaksnr_sobel, snr_sobel)
-[peaksnr_lap4, snr_lap4] = psnr(uint8(rec_img_lap4), mask);
-fprintf("%f, %f\n",peaksnr_lap4, snr_lap4)
-[peaksnr_lap8, snr_lap8] = psnr(uint8(rec_img_lap8), mask);
-fprintf("%f, %f\n",peaksnr_lap8, snr_lap8)
-[peaksnr_log, snr_log] = psnr(uint8(rec_img_log), mask);
-fprintf("%f, %f\n",peaksnr_log, snr_log)
+signal = double(us_img).*(double(mask==255));
+noise = double(us_img).*(double(bg_mask==255));
+SNR=10*log10(sum(sum(signal))/sum(sum(noise)));
+disp(SNR)
+disp(snr(signal,noise))
+signal = double(rec_img_sobel).*(double(mask==255));
+noise = double(rec_img_sobel).*(double(bg_mask==255));
+SNR=10*log10(sum(sum(signal))/sum(sum(noise)));
+disp(SNR)
+disp(snr(signal,noise))
+% [peaksnr_sobel, snr_sobel] = psnr(uint8(rec_img_sobel), bg_mask);
+% fprintf("%f, %f\n",peaksnr_sobel, snr_sobel)
+% [peaksnr_lap4, snr_lap4] = psnr(uint8(rec_img_lap4), bg_mask);
+% fprintf("%f, %f\n",peaksnr_lap4, snr_lap4)
+% [peaksnr_lap8, snr_lap8] = psnr(uint8(rec_img_lap8), bg_mask);
+% fprintf("%f, %f\n",peaksnr_lap8, snr_lap8)
+% [peaksnr_log, snr_log] = psnr(uint8(rec_img_log), bg_mask);
+% fprintf("%f, %f\n",peaksnr_log, snr_log)
